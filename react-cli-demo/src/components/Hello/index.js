e8c82e8013e464607ed129f08b60b1c28c300d66
deanwade9 <546833822@qq.com>
2021-05-08 22:25:57 +0800

    React Todolist init

--- react-cli-demo/src/components/Hello/index.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import './index.css'
-
-// 定义并且暴露Hello组件
-export default class Hello extends Component {
-  render () {
-    return <h2 className="title">Hello React!</h2>
-  }
-}
