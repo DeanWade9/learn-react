2d49c0afd9b21d39d66ff37f407f0b092532b2c9
deanwade9 <546833822@qq.com>
2021-05-09 10:07:47 +0800

    设置defaultChecked属性

--- react-cli-demo/src/components/Item/index.jsx
+++ react-cli-demo/src/components/Item/index.jsx
@@ -7,7 +7,7 @@ export default class Item extends Component {
     return (
       <li>
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" defaultChecked={done}/>
           <span>{ name }</span>
         </label>
         <button className="btn btn-danger" style={{ display: done }}>删除</button>
