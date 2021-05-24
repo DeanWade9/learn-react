f41ccbb185eeeccf0fc5c6be1bc393d11e033bbb
deanwade9 <546833822@qq.com>
2021-05-24 11:04:55 +0800

    deleteTodo删除item

--- react-cli-demo/src/components/Item/index.jsx
+++ react-cli-demo/src/components/Item/index.jsx
@@ -17,7 +17,14 @@ export default class Item extends Component {
     return (event) => {
       // console.log(event.target.checked)
       this.props.updateTodo(id, event.target.checked)
-    } 
+    }
+  }
+
+  // 删除回调
+  handleDelete = (id) => {
+    if(window.confirm('确认删除?')) {
+      this.props.deleteTodo(id)
+    }
   }
 
   render() {
@@ -29,7 +36,7 @@ export default class Item extends Component {
           <input type="checkbox" defaultChecked={done} onChange={this.handleCheck(id)} />
           <span>{ name }</span>
         </label>
-        <button className="btn btn-danger" style={{ display: mouse? 'block' : 'none' }}>删除</button>
+        <button className="btn btn-danger" style={{ display: mouse? 'block' : 'none' }} onClick={() => this.handleDelete(id)} >删除</button>
       </li>
     )
   }
--- react-cli-demo/src/components/List/index.jsx
+++ react-cli-demo/src/components/List/index.jsx
@@ -8,17 +8,18 @@ export default class List extends Component {
   // 对接收的props限制类型和必要性
   static propTypes = {
     todos: PropTypes.array.isRequired,
-    updateTodo: PropTypes.func.isRequired
+    updateTodo: PropTypes.func.isRequired,
+    deleteTodo: PropTypes.func.isRequired
   }
 
   render() {
-    const {todos, updateTodo} = this.props
+    const {todos, updateTodo, deleteTodo} = this.props
     return (
       <ul className="todo-main">
         {
           todos.map(todo => {
             // return <Item key={todo.id} id={todo.id} name={todo.name} done={todo.done} />
-            return <Item key={todo.id} {...todo} updateTodo={updateTodo} />
+            return <Item key={todo.id} {...todo} updateTodo={updateTodo} deleteTodo={deleteTodo} />
           })
         }
       </ul>
