f4cfc692b73e9386748da524efa0b290c2de0bad
deanwade9 <546833822@qq.com>
2021-05-24 09:44:47 +0800

    标识鼠标移入移出

--- react-cli-demo/src/components/Item/index.jsx
+++ react-cli-demo/src/components/Item/index.jsx
@@ -2,15 +2,25 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+
+  state = { mouse: false } // 标识鼠标移入移出
+
+  handleMouse = (flag) => {
+    return () => {
+      this.setState({ mouse: flag })
+    }
+  }
+
   render() {
     const { id, name, done } = this.props
+    const { mouse } = this.state
     return (
-      <li>
+      <li style={{backgroundColor: mouse? '#ddd' : 'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)} >
         <label>
-          <input type="checkbox" defaultChecked={done}/>
+          <input type="checkbox" defaultChecked={done} />
           <span>{ name }</span>
         </label>
-        <button className="btn btn-danger" style={{ display: done }}>删除</button>
+        <button className="btn btn-danger" style={{ display: mouse? 'block' : 'none' }}>删除</button>
       </li>
     )
   }
