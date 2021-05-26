ca5c034f91df299f33d7d19615daf3075f0bd2b1
deanwade9 <546833822@qq.com>
2021-05-26 22:42:42 +0800

    react配置代理setupProxy.js

--- react-cli-demo/src/App.jsx
+++ react-cli-demo/src/App.jsx
@@ -4,16 +4,25 @@ import axios from 'axios'
 export default class App extends Component {
 
   getStudentData = () => {
-    axios.get('http://localhost:3000/students').then(
+    axios.get('http://localhost:3000/api1/students').then(
       response => {console.log('success,data:', response.data)},
       error => {console.log('success,data:', error.error)}
     )
+  }
+
+  getCarData = () => {
+    axios.get('http://localhost:3000/api2/cars').then(res => {
+      console.log(res)
+    }).catch(err => {
+      console.log(err)
+    })
   }
 
   render() {
     return (
       <div>
         <button onClick={this.getStudentData}>点我获取学生数据</button>
+        <button onClick={this.getCarData}>点我获取汽车数据</button>
       </div>
     )
   }
