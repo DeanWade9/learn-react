f67efaffc47280b934bc374bcea91d11438bd892
deanwade9 <546833822@qq.com>
2021-06-26 11:42:06 +0800

    react课程

---
+++ react-cli-demo/react脚手架配置代理2/setupProxy.js
@@ -0,0 +1,17 @@
+// CommonJS
+const proxy = require('http-proxy-middleware')
+
+module.exports = function (app) {
+  app.use(
+    proxy('/api1', {
+      target: 'http://localhost:5000',
+      changeOrigin: true, // 控制服务器收到的请求头中Host字段的值
+      pathRewrite: {'^/api1': ''} // 必须加上
+    }),
+    proxy('/api2', {
+      target: 'http://localhost:5001',
+      changeOrigin: true,
+      pathRewrite: {'^/api2': ''}
+    })
+  )
+}
