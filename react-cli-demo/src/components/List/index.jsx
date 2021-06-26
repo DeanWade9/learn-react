76fba69a1290561e30b22b7b5186ac2232dab479
deanwade9 <546833822@qq.com>
2021-06-26 15:38:23 +0800

    静态页面 拆分组件和样式

---
+++ react-cli-demo/src/components/List/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class List extends Component {
+  render() {
+    return (
+      <div className="row">
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+            <img alt="avatar" src="https://z3.ax1x.com/2021/06/26/R8clzq.jpg" style={{width: '100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+            <img alt="avatar" src="https://z3.ax1x.com/2021/06/26/R8clzq.jpg" style={{width: '100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+            <img alt="avatar" src="https://z3.ax1x.com/2021/06/26/R8clzq.jpg" style={{width: '100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+            <img alt="avatar" src="https://z3.ax1x.com/2021/06/26/R8clzq.jpg" style={{width: '100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+        <div className="card">
+          <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
+            <img alt="avatar" src="https://z3.ax1x.com/2021/06/26/R8clzq.jpg" style={{width: '100px'}}/>
+          </a>
+          <p className="card-text">reactjs</p>
+        </div>
+      </div>
+    )
+  }
+}
