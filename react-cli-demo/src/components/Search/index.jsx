d0d5f08ac57ccb10389cd55af3398b4d1a2e4de3
deanwade9 <546833822@qq.com>
2021-06-27 09:36:33 +0800

    获取用户输入 发送网络请求

--- react-cli-demo/src/components/Search/index.jsx
+++ react-cli-demo/src/components/Search/index.jsx
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Search extends Component {
+
+  search = () => {
+    // 获取用户输入
+    const {keyWordElement: {value: keyword}} = this
+    console.log(keyword)
+    // 发送网络请求
+    axios.get(`http://localhost:3000/api1/search/users?q=${keyword}`).then(
+      response => {console.log('done:', response.data)},
+      error => {console.log('error:',error)}
+    )
+  }
+
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
+          <input ref={c => this.keyWordElement = c} type="text" placeholder="enter the name you search"/>&nbsp;
+          <button onClick={this.search}>Search</button>
         </div>
       </section>
     )
