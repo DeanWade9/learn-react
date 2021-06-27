18cf77f388d11f389693639c8e8c6b2169765279
deanwade9 <546833822@qq.com>
2021-06-27 22:38:40 +0800

    重置

--- react-cli-demo/github搜索案例 fetch/App.jsx
+++ react-cli-demo/github搜索案例 fetch/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import Search from './components/Search'
+import List from './components/List'
 
 export default class App extends Component {
   render() {
     return (
       <div className="container">
-        1024
+        <Search/>
+        <List/>
       </div>
     )
   }
