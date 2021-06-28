0e109097b35bd3db893542235eb557bc0f3ea662
deanwade9 <546833822@qq.com>
2021-06-28 15:29:27 +0800

    刷新样式丢失解决

---
+++ react-cli-demo/react刷新样式丢失问题/App.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { NavLink, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home' // 路由组件
+import About from './pages/About'// 路由组件
+import Header from './components/Header' // 一般组件
+import MyNavLink from './components/MyNavLink'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header a={1}></Header>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* 原生html中 用a标签跳转不同页面 */}
+              {/* <a className="list-group-item" href="./about.html">About</a>
+              <a className="list-group-item active" href="./home.html">Home</a> */}
+
+              {/* react中 靠路由链接实现切换组件 */}
+              {/* <BrowserRouter> */}
+              {/* <NavLink activeClassName="biu" className="list-group-item" to="/about">About</NavLink> */}
+              {/* <NavLink activeClassName="biu" className="list-group-item" to="/home">Home</NavLink> */}
+
+              
+              <MyNavLink to="/dean/about">About</MyNavLink>
+              <MyNavLink to="/dean/home">Home</MyNavLink>
+              {/* </BrowserRouter> */}
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Switch>
+                  <Route path="/dean/about" component={About}></Route>
+                  <Route path="/dean/home" component={Home}></Route>
+                </Switch>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
