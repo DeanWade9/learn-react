300ce988f2bc219b98b8223eeae3faf1a1b6cebe
HongWan PC <546833822@qq.com>
2021-09-20 09:52:50 +0800

    li元素鼠标悬浮效果实现

--- react-cli-demo2/react-demo/src/components/Item/index.jsx
+++ react-cli-demo2/react-demo/src/components/Item/index.jsx
@@ -2,15 +2,24 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class Item extends Component {
+
+  handleMouseEnter = () => {
+    this.refs.btn.style.display = 'block'
+  }
+
+  handleMouseLeave = () => {
+    this.refs.btn.style.display = 'none'
+  }
+
   render() {
-    const {msg, done, id} = this.props
+    const {msg, done} = this.props
     return (
-      <li>
+      <li onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave}>
         <label>
           <input type="checkbox" defaultChecked={done} />
           <span>{ msg }</span>
         </label>
-        <button className="btn btn-danger" style={{ display: 'none' }}>Delete</button>
+        <button ref="btn" className="btn btn-danger" style={{ display: 'none' }}>Delete</button>
       </li>
     )
   }
