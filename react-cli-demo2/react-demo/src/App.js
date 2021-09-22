72288c74a1639a249a71fab3f1ccdb2e63893380
deanwade9 <546833822@qq.com>
2021-09-22 22:30:48 +0800

    完成路由跳转

--- react-cli-demo2/react-demo/src/App.js
+++ react-cli-demo2/react-demo/src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react'
+import { Link, BrowserRouter, Route } from 'react-router-dom'
+import Home from './component/Home'
+import About from './component/About'
 import './app.css'
 
 export default class App extends Component {
@@ -7,15 +10,19 @@ export default class App extends Component {
     return (
       <div className="container">
         <div className="title">React Router Demo</div>
-        <div className="wrapper">
-          <div className="nav">
-            <a className="nav-item">About</a>
-            <a className="nav-item">Home</a>
+        <BrowserRouter>
+          <div className="wrapper">
+            <div className="nav">
+              <Link className="nav-item" to="/about">About</Link>
+              <Link className="nav-item" to="/home">Home</Link>
+            </div>
+            <div className="show">
+              {/* 注册路由 */}
+              <Route path="/about" component={About} />
+              <Route path="/home" component={Home} />
+            </div>
           </div>
-          <div className="show">
-            show
-          </div>
-        </div>
+        </BrowserRouter>
       </div>
     )
   }
