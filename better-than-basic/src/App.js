28a4c5c51de24f6bbf6c06a88990478d940a8f29
deanwade9 <546833822@qq.com>
2022-11-07 23:01:56 +0800

    简单的组件化 Hello组件和Welcome组件

--- better-than-basic/src/App.js
+++ better-than-basic/src/App.js
@@ -1,7 +1,13 @@
 import React from 'react'
-
+import Hello from './components/Hello'
+import Welcome from './components/Welcome'
 export default class App extends React.Component {
   render() {
-    return <div>Hello React 18</div>
+    return (
+      <div>
+        <Hello />
+        <Welcome />
+      </div>
+    )
   }
 }
---
+++ better-than-basic/src/components/Welcome/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Wellcome.css'
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <h2 className="welcome">Welcome</h2>
+    )
+  }
+}
