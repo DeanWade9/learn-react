870b4346c10b3143af2599a66999f4b9b3a0708a
deanwade9 <546833822@qq.com>
2022-11-10 23:23:23 +0800

    拆分组件

--- better-than-basic/src/App.jsx
+++ better-than-basic/src/App.jsx
@@ -1,49 +1,15 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from 'react'
 import './App.css'
+import List from './components/List'
+import Search from './components/Search'
 export default class App extends Component {
   render() {
     return (
       <div>
         <div className="container">
-          <section className="jumbotron">
-            <h3 className="jumbotron-heading">Search Github Users</h3>
-            <div>
-              <input type="text" placeholder="enter the name you search"/>&nbsp;<button>Search</button>
-            </div>
-          </section>
-          <div className="row">
-            <div className="card">
-              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-            <div className="card">
-              <a href="https://github.com/reactjs" target="_blank" rel="noreferrer">
-                <img src="https://avatars.githubusercontent.com/u/6412038?v=3" style={{width: '100px'}}/>
-              </a>
-              <p className="card-text">reactjs</p>
-            </div>
-          </div>
+          <Search />
+          <List />
         </div>
       </div>
     )
