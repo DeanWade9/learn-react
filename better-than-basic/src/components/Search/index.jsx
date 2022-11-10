d8a434cb851fb6637107a27f85ffc0e3254f18b9
deanwade9 <546833822@qq.com>
2022-11-11 00:01:26 +0800

    axios get data

--- better-than-basic/src/components/Search/index.jsx
+++ better-than-basic/src/components/Search/index.jsx
@@ -2,13 +2,24 @@ import React, { Component } from 'react'
 import axios from 'axios'
 
 export default class Search extends Component {
+  state = {
+    userData: []
+  }
   search = () => {
     console.log(this.inputEle.value)
     const {inputEle: {value}} = this
     // axios.get(`https://api.github.com/search/users?q=${value}`).then(res => {
-    axios.get(`http://localhost:3000/api1/search/users?q=${value}`).then(res => {
-      console.log(res)
-    })
+    axios.get(`http://localhost:3000/api1/search/users?q=${value}`).then(
+      res => {
+        console.log(res)
+        if (res.status == 200) {
+          this.props.saveUsers(res.data.items)
+        }
+      },
+      err => {
+        console.log(err)
+      }
+    )
   }
 
   render() {
