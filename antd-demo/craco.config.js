094aca4bcea0793a92152973e63d233d820d7b3c
deanwade9 <546833822@qq.com>
2022-11-13 13:27:52 +0800

    antd 4版本样式按需引入 使用craco

---
+++ antd-demo/craco.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+module.exports = {
+  webpack: {
+    //webpack配置
+    // 配置别名
+    alias: {
+      //设置别名是为了让后续引用的地方减少路径的复杂度
+      '@': path.resolve('src'),
+      '@utils': path.resolve('src/utils'),
+    },
+  },
+  babel: {
+    //支持装饰器
+    plugins: [
+      ['@babel/plugin-proposal-decorators', { legacy: true }],
+      [
+        'import',
+        {
+          libraryName: 'antd',
+          libraryDirectory: 'es',
+          style: 'css', //设置为true即是less 这里用的是css
+        },
+      ],
+    ],
+  },
+}
