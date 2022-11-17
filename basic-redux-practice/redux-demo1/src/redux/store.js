26a253ff6b84eae533353e381b32111f8ee54247
deanwade9 <546833822@qq.com>
2022-11-17 22:13:57 +0800

    引入enhancer redux-thunk 让action支持返回函数

--- basic-redux-practice/redux-demo1/src/redux/store.js
+++ basic-redux-practice/redux-demo1/src/redux/store.js
@@ -1,4 +1,6 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import countReducer from './reducers/count'
+// 让action支持返回函数
+import thunk from 'redux-thunk'
 
-export default createStore(countReducer)
+export default createStore(countReducer, applyMiddleware(thunk))
