3c685b61cae831830f5d76d1ee7cd175842d5756
deanwade9 <546833822@qq.com>
2022-11-17 22:03:20 +0800

    fix action err

--- basic-redux-practice/redux-demo1/src/redux/actions/count.js
+++ basic-redux-practice/redux-demo1/src/redux/actions/count.js
@@ -1,7 +1,7 @@
-import { ADD } from '../constants'
+import { ADD, MINUS } from '../constants'
 
 export const createAddAction = (data) => ({ type: ADD, data })
-export const createMinusAction = (data) => ({ type: ADD, data })
+export const createMinusAction = (data) => ({ type: MINUS, data })
 export const createAsyncAddAction = (data, delay) => {
   return (dispatch) => {
     setTimeout(() => {
