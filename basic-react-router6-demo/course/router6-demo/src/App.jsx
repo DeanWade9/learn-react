774e5f747c09687693d23b5df2cc6bb69e05d3f8
deanwade9 <546833822@qq.com>
2022-11-19 17:28:29 +0800

    react-router6 一级路由和重定向Navigate

---
+++ basic-react-router6-demo/course/router6-demo/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { lazy, Suspense } from 'react'
+import { NavLink, Routes, Route, Navigate } from 'react-router-dom'
+import Loading from './pages/Loading'
+
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
+
+export default function App() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-xs-offset-2 col-xs-8">
+          <div className="page-header">
+            <h2>React Router Demo</h2>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xs-2 col-xs-offset-2">
+          <div className="list-group">
+            {/* 路由链接 */}
+            <NavLink className="list-group-item" to="/about">
+              About
+            </NavLink>
+            <NavLink className="list-group-item" to="/home">
+              Home
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-xs-6">
+          <div className="panel">
+            <div className="panel-body">
+              {/* 注册路由 */}
+              <Suspense fallback={<Loading />}>
+                <Routes>
+                  <Route path="/about" element={<About />}></Route>
+                  <Route path="/home" element={<Home />}></Route>
+                  <Route path="/" element={<Navigate to="/about" />}></Route>
+                </Routes>
+              </Suspense>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
