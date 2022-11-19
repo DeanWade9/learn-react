edbe332e60588af5c928202bc6bdadab16530b36
deanwade9 <546833822@qq.com>
2022-11-19 15:42:11 +0800

    renderProps

--- react-more/src/components/9_renderProps/index.jsx
+++ react-more/src/components/9_renderProps/index.jsx
@@ -2,10 +2,6 @@ import React, { Component } from 'react'
 import './index.css'
 
 export default class TopComp extends Component {
-  state = {
-    name: 'dean',
-  }
-
   render() {
     console.log('comp Top render')
     return (
@@ -15,20 +11,25 @@ export default class TopComp extends Component {
         {/* <A>
           <B></B>
         </A> */}
-        <A render={() => <B />}></A>
+        <A render={(name) => <B name={name} />}></A>
       </div>
     )
   }
 }
 
 class A extends Component {
+  state = {
+    name: 'dean',
+  }
+
   render() {
+    const { name } = this.state
     console.log('comp A render')
     return (
       <div className="A">
         <h3>Comp A</h3>
         {/* {this.props.children} */}
-        {this.props.render()}
+        {this.props.render(name)}
       </div>
     )
   }
@@ -39,7 +40,7 @@ class B extends Component {
     console.log('comp B render')
     return (
       <div className="B">
-        <h3>Comp B</h3>
+        <h3>Comp B {`, name is ${this.props.name}`}</h3>
       </div>
     )
   }
