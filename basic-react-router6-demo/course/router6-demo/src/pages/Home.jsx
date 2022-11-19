7a6e75639a9d4134fbdd00ea49111678932bcf8b
deanwade9 <546833822@qq.com>
2022-11-19 17:41:21 +0800

    重定向Navigate

--- basic-react-router6-demo/course/router6-demo/src/pages/Home.jsx
+++ basic-react-router6-demo/course/router6-demo/src/pages/Home.jsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import { Navigate } from 'react-router-dom'
 
 export default function Home() {
-  return <div>Home</div>
+  const [sum, setSum] = React.useState(1)
+  return (
+    <div>
+      <h3>Home</h3>
+      {/* <h4>Current Sum: {sum}</h4> */}
+      {sum === 2 ? (
+        <Navigate to="/about" replace={false} />
+      ) : (
+        <h4>Current Sum: {sum}</h4>
+      )}
+      <hr />
+      <button onClick={() => setSum(2)}>Change</button>
+    </div>
+  )
 }
