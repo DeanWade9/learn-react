519d038abf67df1c353f5d5333dca352ce854a95
deanwade9 <546833822@qq.com>
2022-11-20 21:28:09 +0800

    useEffect模拟ComponentDidMount和ComponentDidUpdate

--- basic-react-proj/dean-travel/src/App.js
+++ basic-react-proj/dean-travel/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import logo from './logo.svg'
 import './App.css'
+import Demo1 from './components/useEffect-demo'
 
 class App extends React.Component {
   constructor(props) {
@@ -33,22 +33,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
           <div style={{ width: '200px', height: '50px' }}>
             {this.state.count}
           </div>
           <button onClick={this.handleAdd2}>Add</button>
+          <Demo1></Demo1>
         </header>
       </div>
     )
---
+++ basic-react-proj/dean-travel/src/components/useEffect-demo/index.jsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react'
+import { BsCartCheckFill } from 'react-icons/bs'
+
+export default function Demo1() {
+  const [count, setCount] = useState(0)
+  const [robotGallery, setRobotGallery] = useState([])
+
+  useEffect(() => {
+    // console.log('triger useEffect')
+    document.title = `Clicked ${count} times`
+  }, [count])
+
+  // useEffect第二个参数传入空数组 用来模拟ComponentDidMount钩子(只在组件挂载时候执行一次)
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setRobotGallery(data))
+  }, [])
+
+  // useEffect不传入 第二个参数 用来模拟ComponentDidUpdate
+  useEffect(() => {
+    console.log('useEffect')
+  })
+
+  return (
+    <div>
+      <div
+        style={{
+          width: '300px',
+          height: '50px',
+          background: '#ffcc00',
+          lineHeight: '50px',
+        }}
+      >
+        <BsCartCheckFill />
+        <span>Demo1 Count: </span>
+        <span style={{ color: '#111' }}>{count}</span>
+      </div>
+      <button onClick={() => setCount(count + 1)}>Add 1</button>
+      <hr />
+      <div>
+        {robotGallery.map((r) => (
+          <section key={r.id}>
+            <span>id: {r.id}</span>
+            <span>email: {r.email}</span>
+            <span>name: {r.name}</span>
+          </section>
+        ))}
+      </div>
+    </div>
+  )
+}
