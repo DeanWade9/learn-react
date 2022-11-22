ef9e8ea9247cf770a21051e0ae49b8e73e73b7dc
deanwade9 <546833822@qq.com>
2022-11-22 23:21:10 +0800

    functional component 3-4

--- basic-react-proj/robot-gallery/src/App.tsx
+++ basic-react-proj/robot-gallery/src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react'
 import logo from './logo.svg'
 import './App.css'
+import robots from './mockdata/robots.json'
+import Robot from './components/Robot'
 
 function App() {
-  return '11'
-  // <div className="App">
-  //   <header className="App-header">
-  //     <img src={logo} className="App-logo" alt="logo" />
-  //     <p>
-  //       Edit <code>src/App.tsx</code> and save to reload.
-  //     </p>
-  //     <a
-  //       className="App-link"
-  //       href="https://reactjs.org"
-  //       target="_blank"
-  //       rel="noopener noreferrer"
-  //     >
-  //       Learn React
-  //     </a>
-  //   </header>
-  // </div>
+  return (
+    <ul>
+      {robots.map((r) => (
+        <Robot id={r.id} name={r.name} email={r.email} />
+      ))}
+    </ul>
+  )
 }
 
 export default App
