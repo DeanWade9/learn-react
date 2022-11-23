37462b36b512ff7d4aec5fb6f94952d9a905b3ff
deanwade9 <546833822@qq.com>
2022-11-23 23:31:11 +0800

    设置加载字体 @font-face

--- basic-react-proj/robot-gallery/src/App.tsx
+++ basic-react-proj/robot-gallery/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from './logo.svg'
+import logo from '../src/assets/images/logo.svg'
 import style from './App.module.css'
 import robots from './mockdata/robots.json'
 import Robot from './components/Robot'
@@ -7,6 +7,10 @@ import Robot from './components/Robot'
 function App() {
   return (
     <div className={style.app}>
+      <div className={style.appHeader}>
+        <img src={logo} alt="logo" className={style.appLogo} />
+        <h1>Robot Shpping Center Online</h1>
+      </div>
       <div className={style.robotList}>
         {robots.map((r) => (
           <Robot id={r.id} name={r.name} email={r.email} />
